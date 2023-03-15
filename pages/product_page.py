1729eb84fb819cccb2feeb356892a414fa017d6d
Mikhail Sokolik <sokolikmichael@yandex.ru>
2023-03-15 10:54:20 +0400

    Finding 1 bugged page from 10

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,23 +4,29 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def should_be_product_page(self):
-        # self.should_be_product_page_url()
+        self.should_be_product_page_url()
         self.should_be_add_to_basket_button()
 
-    # def should_be_product_page_url(self):
-    #     assert "?promo=newYear" in self.browser.current_url, "Not a product page url"
+    def should_be_product_page_url(self):
+        assert "?promo=offer" in self.browser.current_url, "Not a product page url"
 
     def should_be_add_to_basket_button(self):
         assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), "No 'Add to basket' button"
 
     def click_add_to_basket(self):
+        self.should_be_add_to_basket_button()
         add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_to_basket_button.click()
 
     def product_name_should_be_correct(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text in self.browser.find_element(
+        assert self.browser.find_element(
+            *ProductPageLocators.PRODUCT_NAME).text + " has been added to your basket." == self.browser.find_element(
             *ProductPageLocators.SUCCESS_ALERT).text, "Wrong product name !!!"
 
     def product_price_should_be_correct(self):
         assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text in self.browser.find_element(
             *ProductPageLocators.BASKET_ALERT).text, "Wrong price !!!"
+
+    def added_to_basket_correctly(self):
+        self.product_name_should_be_correct()
+        self.product_price_should_be_correct()
